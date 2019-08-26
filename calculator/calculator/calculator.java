9e1f14813833e59ad1f61fa9fa19c667275460ee
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-08-26 10:49:05 +0600

    java program with addition

---
+++ calculator/calculator/calculator.java
@@ -0,0 +1,32 @@
+package calculator;
+import java.util.Scanner;
+public class calculator {
+
+	public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter First numbers: ");
+        double first = reader.nextDouble();
+        
+        System.out.print("Enter Second numbers: ");
+        double second = reader.nextDouble();
+
+        System.out.print("Enter operator (+): ");
+        char operator = reader.next().charAt(0);
+
+        double result;
+
+        switch(operator)
+        {
+            case '+':
+                result = first + second;
+                break;
+
+            default:
+                System.out.printf("Error! operator is not correct");
+                return;
+        }
+
+        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+    }
+}
